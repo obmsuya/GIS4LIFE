27517969dbdcd13a75869ea3583ad59da1fb35b4
obmsuya <obmsuya@yahoo.com>
2018-02-15 10:15:21 +0300

    static set outside the project folder

--- mysite/settings.py
+++ mysite/settings.py
@@ -128,7 +128,7 @@ STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 
 MEDIA_ROOT =  os.path.join(BASE_DIR, 'mysite/media')
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'static')
 
 
 LOGIN_EXEMPT_URLS = (
